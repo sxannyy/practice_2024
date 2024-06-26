b42c5712bd554c8735726c57a16f3fe8a4c72c02
sxannyy <sxannyy@mail.ru>
2024-06-27 03:07:10 +0800

    Added test for creating, getting, updating users. Also added test for role management. Fixed alembic bugs and added test database

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,17 +13,17 @@ wait
 alembic upgrade heads
 wait
 
-# cd tests
+cd tests
 
-# export PYTHONPATH=$loc_path:$PYTHONPATH
+export PYTHONPATH=$loc_path:$PYTHONPATH
 
-# alembic init migrations
-# wait
+alembic init migrations
+wait
 
-# sed -i "s/^# from myapp import mymodel/from db.models import Base/" ./migrations/env.py
-# sed -i "s/target_metadata = None/target_metadata = Base.metadata/" ./migrations/env.py
+sed -i "s/^# from myapp import mymodel/from db.models import Base/" ./migrations/env.py
+sed -i "s/target_metadata = None/target_metadata = Base.metadata/" ./migrations/env.py
 
-# alembic revision --autogenerate -m "init_db"
-# wait
-# alembic upgrade heads
-# wait
+alembic revision --autogenerate -m "init_db"
+wait
+alembic upgrade heads
+wait
--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
 ALGORITHM: str = env.str("ALGORITHM", default="HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
 
-# TEST_DATABASE_URL = env.str(
-#     "TEST_DATABASE_URL",
-#     default="postgresql+asyncpg://sxannyy_test:7721_test@0.0.0.0:5433/practice_test"
-# )
+TEST_DATABASE_URL = env.str(
+    "TEST_DATABASE_URL",
+    default="postgresql+asyncpg://sxannyy_test:7721_test@0.0.0.0:5433/practice_test"
+)
